c4f0fc5d06b4a328adeb2fe6a893671332e60715
Arne Brasseur <arne@arnebrasseur.net>
2017-03-09 11:55:26 +0100

    Add missing dependency

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject lambdaisland/garden-watcher "0.3.0"
+(defproject lambdaisland/garden-watcher "0.3.1"
   :description "A component for reloading Garden stylesheets"
-  :url "http://example.com/plexus/garden-watcher"
+  :url "https://github.com/plexus/garden-watcher"
   :license {:name "Mozilla Public License 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.stuartsierra/component "0.3.1"]
+                 [org.clojure/java.classpath "0.2.3"]
                  [hawk "0.2.11"]
                  [garden "1.3.2"]])
