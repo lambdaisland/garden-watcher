129f1489f6d82b143ed9ff4561cc24a1fe1ab7b0
Arne Brasseur <arne@arnebrasseur.net>
2018-10-15 15:43:29 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/plexus/garden-watcher"
   :license {:name "Mozilla Public License 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.stuartsierra/component "0.3.1"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.stuartsierra/component "0.3.2"]
                  [org.clojure/java.classpath "0.3.0"]
                  [hawk "0.2.11"]
-                 [garden "1.3.2"]])
+                 [garden "1.3.6"]])
